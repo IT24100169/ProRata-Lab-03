3939aafaaa4137d00cbb54dbc81474665a9d840b
IT24100169 <IT24100169@my.sliit.lk>
2026-09-18 20:15:14 +0530

    IT24100169_Lab3_Q1A

---
+++ Q1A/IT24100169Lab3Q1A.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class IT24100169Lab3Q1A {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the price of 1kg of rice: ");
+        double pricePerKg = input.nextDouble();
+
+        System.out.print("Enter the number of kilograms you want to buy: ");
+        double kilograms = input.nextDouble();
+
+        double total = pricePerKg * kilograms;
+
+        System.out.println("The total amount is: " + total);
+
+        
+    }
+}
